be32c6bcc1048e3b9f60c68352df848d7abdb2b9
Adi-K-Coding <Adityakhandelwal2006@gmail.com>
2022-03-13 23:20:22 -0700

    menu stuff

--- design.py
+++ design.py
@@ -1,9 +1,22 @@
 def design():
-    str1 = "      "
-    str2 = "*"
-    for i in range(1, 6):
-        print(str1 + (str2 * i) + str1)
-        str1 = slice(0, len(str1) - 2, 1)
+    treesize = input("How large would you like your tree to be:  ")
+    treesizeint = int(treesize)
+    print(" ")
+    str1 = " " * treesizeint
+    str2 = "* "
+    for i in range(1, treesizeint):
+        print(str1 + (str2 * i))
+        str1 = str1[0: len(str1) - 1]
+
+    trunkspace = (treesizeint / 2)
+    trunkspaceint = int(trunkspace)
+    spcs = " " * treesizeint
+
+    print("", end="")
+    print(spcs, end="* * ")
+    print("")
+    print("", end="")
+    print(spcs, end="* * ")
 
 
 if __name__ == "__main__":
--- week_one.py
+++ week_one.py
@@ -1,9 +1,13 @@
 import swapper
 import matrix
+import design
+import funcy
 
 main_menu = [
     ["swap", "swapper.py"],
     ["keyboard", "matrix.py"],
+    ["design", "design.py"],
+    ["animation", "funcy.py"],
     ["exit", "c"],
 ]
 
@@ -22,7 +26,13 @@ if __name__ == "__main__":
         elif userInput == "keyboard" or userInput == "2":
             print(" ")
             matrix.matrix1()
-        elif userInput == "exit" or userInput == "3":
+        elif userInput == "design" or userInput == "3":
+            print(" ")
+            design.design()
+        elif userInput == "animation" or userInput == "4":
+            print(" ")
+            funcy.monkey()
+        elif userInput == "exit" or userInput == "5":
             exit()
         else:
             print("Enter a valid input")
